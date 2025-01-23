c144427dce514b074ed73f13ffd28c5cebbec64a
BychinskyDO <DBychinsky@qulix.com>
2025-01-23 14:27:39 +0300

    добавлен код для отключения типа камеры

--- src/components/qr/Qr.tsx
+++ src/components/qr/Qr.tsx
@@ -5,25 +5,72 @@ function Qr() {
 
     const [scanResult, setScanResult] = useState('');
 
+    // Если понадобится отключение выбора камеры при старте сканирования
+    // ВОзможно стоит поискать нормальные решения
+    // useEffect(() => {
+    //     const scannerMobile = new Html5QrcodeScanner('reader', {
+    //         qrbox: {
+    //             width: 250,
+    //             height: 250,
+    //         },
+    //         fps: 20,
+    //         // supportedScanTypes: [
+    //         //     Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+    //         videoConstraints: {
+    //             facingMode: {exact: "environment"},
+    //         },
+    //     }, undefined);
+    //
+    //     const scannerDesctop = new Html5QrcodeScanner('reader', {
+    //         qrbox: {
+    //             width: 250,
+    //             height: 250,
+    //         },
+    //         fps: 20,
+    //     }, undefined);
+    //
+    //     async function getMedia() {
+    //         try {
+    //             return await navigator.mediaDevices.getUserMedia();
+    //             /* use the stream */
+    //         } catch (err) {
+    //             /* handle the error */
+    //         }
+    //     }
+    //
+    //     getMedia().then((response) => {
+    //         if (response) {
+    //             scannerMobile.render(success, error)
+    //         } else {
+    //             scannerDesctop.render(success, error)
+    //         }
+    //     })
+    //
+    //
+    //     function success(result: string) {
+    //         setScanResult(result);
+    //         scannerMobile.clear();
+    //     }
+    //
+    //     function error() {
+    //         console.log('error');
+    //     }
+    // }, []);
+
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner('reader', {
+        const scannerMobile = new Html5QrcodeScanner('reader', {
             qrbox: {
                 width: 250,
                 height: 250,
             },
             fps: 20,
-            // supportedScanTypes: [
-            //     Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-            videoConstraints: {
-                facingMode: {exact: "environment"},
-            },
         }, undefined);
 
-        scanner.render(success, error)
+        scannerMobile.render(success, error)
 
         function success(result: string) {
             setScanResult(result);
-            scanner.clear();
+            scannerMobile.clear();
         }
 
         function error() {
